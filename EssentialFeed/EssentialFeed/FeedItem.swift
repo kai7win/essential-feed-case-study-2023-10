9b63d447a1145bf182b0945fb358b9c0e17a392b
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-10-20 19:45:45 +0800

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Kai Chi Tsao on 2023/10/20.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
