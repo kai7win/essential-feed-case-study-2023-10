6c15fd6c75adf8fa8f5c2457c934129f7ec5a566
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-12-17 07:32:30 +0800

    Extract duplicate test helpers into a shared scope

---
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,17 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Kai Chi Tsao on 2023/12/17.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
