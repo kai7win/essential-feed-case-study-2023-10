250e4549bcf2dba084d9f86cf864ef460ed39b64
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-12-19 06:21:09 +0800

    Extract `FailableRetrieveFeedStoreSpecs` checks into reusable assertions

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -50,7 +50,7 @@ class CodableFeedStoreTests:XCTestCase, FailableFeedStoreSpecs{
         
         try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
         
-        expect(sut, toRetrieve: .failure(anyNSError()))
+        assertThatRetrieveDeliversFailureOnRetrievalError(on: sut)
     }
     
     
@@ -60,7 +60,7 @@ class CodableFeedStoreTests:XCTestCase, FailableFeedStoreSpecs{
         
         try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
         
-        expect(sut, toRetrieveTwice: .failure(anyNSError()))
+        assertThatRetrieveHasNoSideEffectsOnFailure(on: sut)
         
     }
     
