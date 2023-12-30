679d6a33cf26bfe18ea6ab9c99031fa19420e39d
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-12-30 20:26:10 +0800

    Add `FeedPresenter` holding feed loading presentation logic (loading state and loaded feed)

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -6,33 +6,33 @@
 //
 
 import UIKit
-import EssentialFeed
 
-final class FeedRefreshViewController: NSObject {
+final class FeedRefreshViewController: NSObject, FeedLoadingView {
     
-    private(set) lazy var view = binded(UIRefreshControl())
-    private let viewModel: FeedViewModel
+    private(set) lazy var view = loadView()
+    private let presenter: FeedPresenter
     
-    init(viewModel: FeedViewModel) {
-        self.viewModel = viewModel
+    init(presenter: FeedPresenter) {
+        self.presenter = presenter
     }
     
-    var onRefresh: (([FeedImage]) -> Void)?
     
     @objc func refresh() {
-        viewModel.loadFeed()
+        presenter.loadFeed()
     }
     
-    private func binded(_ view: UIRefreshControl) -> UIRefreshControl {
-        viewModel.onLoadingStateChange = { [weak view] isLoading in
-            if isLoading {
-                view?.beginRefreshing()
-            } else {
-                view?.endRefreshing()
-            }
+    func display(isLoading: Bool) {
+        if isLoading {
+            view.beginRefreshing()
+        } else {
+            view.endRefreshing()
         }
+    }
+    
+    private func loadView() -> UIRefreshControl {
+        let view = UIRefreshControl()
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
         return view
     }
-     
+    
 }
