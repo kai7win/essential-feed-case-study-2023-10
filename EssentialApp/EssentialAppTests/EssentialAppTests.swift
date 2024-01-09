480b6ce7eb9fd2751a46c74a11846d9dbfe1e3fe
KaiChi <34007164+kai7win@users.noreply.github.com>
2024-01-09 09:08:57 +0800

    Create EssentialApp workspace combining EssentialApp and EssentialFeed projects

---
+++ EssentialApp/EssentialAppTests/EssentialAppTests.swift
@@ -0,0 +1,11 @@
+//
+//  EssentialAppTests.swift
+//  EssentialAppTests
+//
+//  Created by Kai Chi Tsao on 2024/1/9.
+//
+
+import XCTest
+@testable import EssentialApp
+
+class EssentialAppTests: XCTestCase {}
