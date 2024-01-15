65be9baf3f0504a5152e64a2a4bc58ab2f797e22
KaiChi <34007164+kai7win@users.noreply.github.com>
2024-01-15 14:40:40 +0800

    Add ImageComment data model

---
+++ EssentialFeed/EssentialFeed/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,23 @@
+//
+//  ImageComment.swift
+//  EssentialFeed
+//
+//  Created by Kai Chi Tsao on 2024/1/15.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
+
