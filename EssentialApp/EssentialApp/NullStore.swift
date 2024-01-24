b736b9ce6626ae41d997f17bf472ce2b263c6604
KaiChi <34007164+kai7win@users.noreply.github.com>
2024-01-25 07:07:58 +0800

    Group protocol conformance in extensions

--- EssentialApp/EssentialApp/NullStore.swift
+++ EssentialApp/EssentialApp/NullStore.swift
@@ -8,7 +8,9 @@
 import Foundation
 import EssentialFeed
 
-class NullStore: FeedStore & FeedImageDataStore {
+class NullStore {}
+
+extension NullStore: FeedStore {
     func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         completion(.success(()))
     }
@@ -20,7 +22,9 @@ class NullStore: FeedStore & FeedImageDataStore {
     func retrieve(completion: @escaping RetrievalCompletion) {
         completion(.success(.none))
     }
-    
+}
+
+extension NullStore: FeedImageDataStore {
     func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
         completion(.success(()))
     }
